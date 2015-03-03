7f80dffc82dd9cb2ccc677410232d35fc060c7ea
Leandro Cerencio <cerencio@yahoo.com.br>
2015-03-03 18:56:39 -0300

    setup.py in accordance with pypi repository

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+classifiers=['Development Status :: 4 - Beta',
+             'Environment :: X11 Applications :: GTK',
+             'Intended Audience :: End Users/Desktop',
+             'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+             'Operating System :: OS Independent',
+             'Programming Language :: Python',
+             'Topic :: Desktop Environment :: Window Managers :: Applets']
+
 setup(name='caps-lock',
       version='1.0',
       description='Keyboard Status Applet',
